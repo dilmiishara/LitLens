45f781ca061ebfc79cac7e88f785d32cd49eac6c
dilmiishara2002 <dilmiishara2000@gmail.com>
2024-12-24 06:17:17 +0530

    feat: add BookModel component for displaying detailed book information in a modal

--- frontend/src/components/home/BookSingleCard.jsx
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -2,12 +2,15 @@ import { BookOnlineOutlined } from "@mui/icons-material";
 import React from "react";
 import { Link } from 'react-router-dom';
 import { PiBookOpenTextLight } from 'react-icons/pi';
-import { BiUserCircle } from 'react-icons/bi';
+import { BiUserCircle, BiShow } from 'react-icons/bi';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineDelete } from 'react-icons/md'; // Added missing import
+import { useState } from "react";
+import BookModel from "./BookModel";
 
 const BookSingleCard = ({ book }) => {
+    const [showModal,setShowModal]= useState(false);
   return (
     <div
       key={BookOnlineOutlined._id}
@@ -32,6 +35,9 @@ const BookSingleCard = ({ book }) => {
         <h4 className="my-2 text-gray-500">{book.reviewText}</h4>
       </div>
       <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
+        <BiShow
+        className="text-3xl text-blue-800 hover:text-black cursor-pointer"
+        onClick={()=> setShowModal(true)}/>
         <Link to={`/reviews/details/${book._id}`}>
           <BsInfoCircle className="text-2xl text-green-800" />
         </Link>
@@ -42,6 +48,9 @@ const BookSingleCard = ({ book }) => {
           <MdOutlineDelete className="text-2xl text-red-600" />
         </Link>
       </div>
+      {showModal && (
+            <BookModel book={book} onClose={()=> setShowModal(false)}/>
+        )}
     </div>
   );
 };
